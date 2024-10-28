5e93e66adb4ec673da43d287b5cf26ed7e4656e6
Undecane <undecane@outlook.com>
2024-10-28 02:44:29 +0000

    wip

--- tests/DynamicLoggerTest.php
+++ tests/DynamicLoggerTest.php
@@ -12,12 +12,12 @@ final class DynamicLoggerTest extends TestCase
 {
     public function testLoggerUsesCurrent()
     {
-        $mock = $this->getMockBuilder(YiiLogger::class)->getMock();
+        $mock = $this->getMockBuilder(YiiLogger::className())->getMock();
         $mock->expects($this->once())
             ->method('log')
             ->with('test1 []', YiiLogger::LEVEL_INFO);
 
-        $yiiLogger2 = $this->getMockBuilder(YiiLogger::class)->getMock();
+        $yiiLogger2 = $this->getMockBuilder(YiiLogger::className())->getMock();
         $yiiLogger2->expects($this->once())
             ->method('log')
             ->with('test2 []', YiiLogger::LEVEL_INFO);
