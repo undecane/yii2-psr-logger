65577143a429b8b4451037659e08d721aff2f49f
Undecane <undecane@outlook.com>
2024-10-28 06:06:18 +0000

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,10 @@
 
 namespace Zing\YiiPsrLogger\Tests;
 
+if (class_exists('PHPUnit_Framework_TestCase') && ! class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
+
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
