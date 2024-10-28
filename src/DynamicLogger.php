a1597139aad62b154db41a265c783444cd6e6642
Undecane <undecane@outlook.com>
2024-10-28 06:00:44 +0000

    wip

--- src/DynamicLogger.php
+++ src/DynamicLogger.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zing\YiiPsrLogger;
 
 use Psr\Log\AbstractLogger;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zing\YiiPsrLogger\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
