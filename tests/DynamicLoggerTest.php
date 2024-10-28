034c0ae87870fbad94a12f06fdfd92838b961330
Undecane <undecane@outlook.com>
2024-10-28 02:29:46 +0000

    wip

--- tests/DynamicLoggerTest.php
+++ tests/DynamicLoggerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zing\YiiPsrLogger\Tests;
 
 use yii\log\Logger as YiiLogger;
--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zing\YiiPsrLogger\Tests;
 
 use Psr\Log\LogLevel;
